f3a4d15e0067c6bb6eb749d94bafbd7233b5f3fb
Nikhil Nar <nik.nar123@gmail.com>
2019-12-02 21:29:24 -0500

    Added code for magic_code

--- magic_code_client.c
+++ magic_code_client.c
@@ -6,42 +6,50 @@
 
 #include "magic_code.h"
 
-
-void
-magic_code_prog_1(char *host)
+void magic_code_prog_1(char *host, char *magic_code_1_arg;)
 {
 	CLIENT *clnt;
-	void  *result_1;
-	char * magic_code_1_arg;
+	void *result_1;
 
-#ifndef	DEBUG
-	clnt = clnt_create (host, MAGIC_CODE_PROG, MAGIC_CODE_VERS, "udp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
+#ifndef DEBUG
+	clnt = clnt_create(host, MAGIC_CODE_PROG, MAGIC_CODE_VERS, "udp");
+	if (clnt == NULL)
+	{
+		clnt_pcreateerror(host);
+		exit(1);
 	}
-#endif	/* DEBUG */
-    magic_code_1_arg = "C00lMessage";
+#endif /* DEBUG */
 	result_1 = magic_code_1(&magic_code_1_arg, clnt);
-	if (result_1 == (void *) NULL) {
-		clnt_perror (clnt, "call failed");
+	if (result_1 == (void *)NULL)
+	{
+		clnt_perror(clnt, "call failed");
 	}
-#ifndef	DEBUG
-	clnt_destroy (clnt);
-#endif	 /* DEBUG */
+#ifndef DEBUG
+	clnt_destroy(clnt);
+#endif /* DEBUG */
 }
 
-
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char *host;
+	char line[1024];
 
-	if (argc < 2) {
-		printf ("usage: %s server_host\n", argv[0]);
-		exit (1);
+	if (argc < 2)
+	{
+		printf("usage: %s server_host\n", argv[0]);
+		exit(1);
 	}
 	host = argv[1];
-	magic_code_prog_1 (host);
-exit (0);
+	while (1)
+	{
+		printf("Enter message =");
+		gets(line);
+		// if the input is exit then exit the client and server code
+		if (strstr(line, "C00l"))
+		{
+			magic_code_prog_1(host, line), ;
+			printf("Message sent to the server\n");
+		}
+	}
+	exit(0);
 }
--- magic_code_server.c
+++ magic_code_server.c
@@ -5,17 +5,38 @@
  */
 
 #include "magic_code.h"
+#include <signal.h>
+
+int noOfDigits = 0;
+int noOfLines = 0;
+
+void sigintHandler(int sig_num)
+{
+	printf("Total number of lines = %d", noOfLines);
+	printf("\nTotal number of digits = %d", noOfDigits);
+	printf("\n");
+	exit(0);
+}
 
 void *
 magic_code_1_svc(char **argp, struct svc_req *rqstp)
 {
-	static char * result;
+	static char *result;
 
 	/*
 	 * insert server code here
 	 */
-    printf("Remote procedure called now =====\n");
-    printf("Remote procedure called \n");
+	printf("Remote procedure called now =====\n");
+	printf("Remote procedure called %s\n", *argp);
+	signal(SIGINT, sigintHandler);
+	char *s;
+	for (s = *argp; *s != NULL; s++)
+	{
+		if (isdigit(*s))
+			noOfDigits++;
+	}
 
-	return (void *) &result;
+	noOfLines++;
+
+	return (void *)&result;
 }
